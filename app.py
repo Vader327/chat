7fd0ecfcd83a1b03636b99fed7e83ec62c808395
Saaheer Purav <saaheer.purav@gmail.com>
2021-06-04 23:49:35 +0530

    Added method of grouping messages with the same sender

--- app.py
+++ app.py
@@ -272,4 +272,5 @@ def send_status(json):
 
 if __name__ == "__main__":
   app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-  socketio.run(app, debug=True)
+  #socketio.run(app, debug=True)
+  socketio.run(app, debug=True, host="0.0.0.0")
